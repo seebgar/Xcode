83c5b7897b61350a0228e92d4025f482847a9c05
Sebastian Garcia <sebastiangarcia@Sebastians-MacBook-Pro-3.local>
2018-05-31 10:17:48 -0500

    base

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Sebastian Garcia on 5/22/18.
+//  Copyright © 2018 seebgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
